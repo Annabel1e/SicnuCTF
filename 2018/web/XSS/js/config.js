5be76cba282a14f38d7dc2a1817e68aff1bb7faa
slpw <765418608@qq.com>
2018-05-08 16:38:43 +0800

    Create config.js

---
+++ 2018/web/XSS/js/config.js
@@ -0,0 +1,14 @@
+var sicnus = {
+	'sicnu':[
+		'welcome to sicnuctf'
+	],
+	'uncis':[
+		'welcome to ftcuncis'	
+	],
+	'sssss':[
+		'sssssssssssssssssss'
+	],
+	'flag':[
+		'fl♂ag{Th15_ls_N♂t_FI4g}'
+	],
+}
